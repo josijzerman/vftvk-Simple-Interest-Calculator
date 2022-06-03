800516a9e6b4d0735b5bcea0cd12686a71c586bc
ijzerman <jos.ijzerman@nlr.nl>
2022-06-03 18:29:31 +0200

    assingments

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 function compute()
-{
-    p = document.getElementById("principal").value;
-    
+{ 
+  var principal = document.getElementById("principal").value;
+  var rate = document.getElementById("rate").value;
+  var years = document.getElementById("years").value;
+
+  var year = new Date().getFullYear()+parseInt(years);
+  var amount = principal*Math.pow((1.0 + (rate /100.0)), years);
+
+  if (principal == "")
+  {
+    var result_html = "<p>Invalid Input</p>";
+    document.getElementById("result").innerHTML = result_html;
+  }
+  else
+  {
+    var result_html = "<p>If you deposit <b>" + principal + "</b>,</p><p>at an interest rate of <b>"+ rate + " %</b>.</p><p>You will receive an amount of <b>" + amount.toFixed(2) + ",</b></p><p>in the year <b>" + year + "</b></p>"
+    document.getElementById("result").innerHTML = result_html;
+  }
+
 }
         
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
